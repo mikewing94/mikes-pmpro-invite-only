1d47b25c1e2338b2659546036dc6e020383a1ebf
mikewing94 <michaelwing@Michaels-Mac-Pro.local>
2017-11-09 12:20:41 +0000

    code for displaying the table on the site

---
+++ usage.php
@@ -0,0 +1,6 @@
+<?php
+include '/wp-content/plugins/mikes-pmpro-invite-only/mikes-pmpro-invite-only.php';
+
+_e('Used Invite Codes', 'pmpro_invite_only');
+echo pmproio_displayInviteCodes(21, false, true);
+?>

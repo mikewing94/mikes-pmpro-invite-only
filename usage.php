5502d4273386d46ad5c728492e104553f2d194ce
Mike Wing <michael.wing@rocketmail.com>
2017-11-09 12:23:18 +0000

    Updated usage.php
    Working dependant on currently logged in user

--- usage.php
+++ usage.php
@@ -2,5 +2,5 @@
 include '/wp-content/plugins/mikes-pmpro-invite-only/mikes-pmpro-invite-only.php';
 
 _e('Used Invite Codes', 'pmpro_invite_only');
-echo pmproio_displayInviteCodes(21, false, true);
+echo pmproio_displayInviteCodes($current_user->ID, false, true);
 ?>
